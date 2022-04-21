3180e19236ede3dafd358a3d9edcb654bbfb264a
danbai <danbai@88.com>
2022-04-22 00:12:56 +0800

    build

--- http/check.go
+++ http/check.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	logs "github.com/danbai225/go-logs"
 	"github.com/gogf/gf/v2/net/ghttp"
 	"net/http"
 	"search-trace-server/server"
@@ -10,11 +11,13 @@ const v1TokenKey string = "_token"
 
 func checkV1(r *ghttp.Request) {
 	token := r.GetHeader(v1TokenKey)
+	logs.Info(token, 14)
 	if token == "" {
 		r.Response.WriteStatus(http.StatusUnauthorized)
 		return
 	}
 	user, err := server.UserGetUserByToken(token)
+	logs.Info(user, err, 20)
 	if err != nil {
 		r.Response.WriteStatus(http.StatusUnauthorized)
 		return
