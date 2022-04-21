61e6932d726ea8fc9afe11b444275137db7bae17
danbai <danbai@88.com>
2022-04-22 01:05:16 +0800

    build

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ if [[ $tag == "" ]]
 then
     tag="build"
 fi
-docker build -t search-trace-server:"$tag" .
+docker build -t danbai225/search-trace-server:"$tag" .
 #docker push danbai225/search-trace-server:"$tag"
