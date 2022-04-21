709021103b60e64717f4e2b0bd95ae455bc55b8e
danbai <danbai@88.com>
2022-04-21 17:45:08 +0800

    build

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 git pull
 tag=$(git describe --abbrev=0)
-
-if [[ tag =~ "fatal" ]]
+if [[ $tag == "" ]]
 then
-    echo tag="build"
+    tag="build"
 fi
 docker build -t search-trace-server:"$tag" .
 docker push danbai225/search-trace-server:"$tag"
