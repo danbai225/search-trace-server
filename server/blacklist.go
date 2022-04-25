411bcd3f28f978e00dee2856266598526543f6ab
danbai <danbai@88.com>
2022-04-25 22:10:11 +0800

    快捷添加

--- server/blacklist.go
+++ server/blacklist.go
@@ -90,7 +90,7 @@ func BlacklistDelDomain(domain string) (err error) {
 			tx.Callback()
 		}
 	}()
-	domain = fmt.Sprint("*://", domain, "/*")
+	domain = fmt.Sprint("*://", domain, "*")
 	blacklist := model.Blacklist{}
 	err = tx.Model(&blacklist).Where("name=?", "domain").First(&blacklist).Error
 	blacklist.Name = "domain"
